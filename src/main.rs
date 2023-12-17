0b2d1935b96133493b6e089c23e1012f86f87fd8
solomonbaez <solomon.james.baez@gmail.com>
2023-12-17 14:01:55 -0800

    Added naive file loading -> included sample hello.txt file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let flags: Vec<String> = env::args().collect();
+    if flags.len() < 3 {
+        panic!("input error: please input searchstring and filepath")
+    }
+
+    let query = &flags[1];
+    let path = File::open(&flags[2]).unwrap();
+
+    let _reader = BufReader::new(&path);
+
+    println!("Searching for {}, in {:?}", query, path);
 }
