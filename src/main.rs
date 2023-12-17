0aae050471b15109dd825f2b9c15818481d00e3a
solomonbaez <solomon.james.baez@gmail.com>
2023-12-17 15:22:55 -0800

    Significantly improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,43 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::io::{Error, ErrorKind};
 
 fn main() {
-    let flags: Vec<String> = env::args().collect();
-    if flags.len() != 3 {
-        panic!("input error: please provide a query and a filepath");
-    }
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(args).unwrap_or_else(|e| {
+        println!("failed to parse arguments - {}", e);
+        std::process::exit(1)
+    });
 
-    let config = Config::new(flags).unwrap();
-
-    let reader = BufReader::new(&config.path);
-    reader.lines().enumerate().for_each(|(line_index, line)| {
+    let contents = BufReader::new(&config.file).lines().enumerate();
+    contents.for_each(|(line_index, line)| {
         if line.unwrap().contains(&config.query) {
             println!("{} found in file at line {}", config.query, line_index);
         }
     });
 
-    println!("Searching for {}, in {:?}", config.query, config.path);
+    println!("Searching for {}, in {:?}", config.query, config.file);
 }
 
 struct Config {
     query: String,
-    path: File,
+    file: File,
 }
 
 impl Config {
-    fn new(flags: Vec<String>) -> Result<Config, std::io::Error> {
-        let query = flags[1].to_string();
-        let path = File::open(&flags[2])?;
+    fn build(args: Vec<String>) -> Result<Config, std::io::Error> {
+        if args.len() != 3 {
+            return Err(Error::new(
+                ErrorKind::Other,
+                "input error: please provide a query string and a filepath",
+            ));
+        }
+        let query = args[1].to_string();
 
-        Ok(Config { query, path })
+        match File::open(&args[2]) {
+            Ok(file) => Ok(Config { query, file }),
+            Err(e) => Err(e),
+        }
     }
 }
