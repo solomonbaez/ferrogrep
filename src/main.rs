bc80789eaa3853fe2b496a4467516482c2c3129e
solomonbaez <solomon.james.baez@gmail.com>
2023-12-17 15:04:27 -0800

    Added config structure and constructor

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,32 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     let flags: Vec<String> = env::args().collect();
-    if flags.len() < 3 {
-        panic!("input error: please input searchstring and filepath")
+    if flags.len() != 3 {
+        panic!("input error: please provide a query and a filepath");
     }
 
-    let query = &flags[1];
-    let path = File::open(&flags[2]).unwrap();
+    let config = Config::new(flags).unwrap();
 
-    let reader = BufReader::new(&path);
+    let reader = BufReader::new(&config.path);
     reader.lines().enumerate().for_each(|(line_index, line)| {
-        if line.unwrap().contains(query) {
-            println!("{} found in file at line {}", query, line_index);
+        if line.unwrap().contains(&config.query) {
+            println!("{} found in file at line {}", config.query, line_index);
         }
     });
 
-    println!("Searching for {}, in {:?}", query, path);
+    println!("Searching for {}, in {:?}", config.query, config.path);
+}
+
+struct Config {
+    query: String,
+    path: File,
+}
+
+impl Config {
+    fn new(flags: Vec<String>) -> Result<Config, std::io::Error> {
+        let query = flags[1].to_string();
+        let path = File::open(&flags[2])?;
+
+        Ok(Config { query, path })
+    }
 }
